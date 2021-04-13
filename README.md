f96ccda8ec2c42890a5ed5fa0bc6b5ba06821021
hakobpogh <hakobpogh@gmail.com>
2021-04-13 12:06:34 +0400

    [add] webpack for build
     # other changes
     * [add] initial documentation

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # light-join
+
+Join urls or paths into single path.
+
+## installation
+
+with npm
+```
+npm i -s light-join
+```
+
+with yarn
+```
+yarn add light-join
+```
+
+## usage
+
+JavaScript
+```javascript
+import lightJoin from 'light-join';
+
+lightJoin('/your-base/url/', 'some/path', '/some-other/path');
+```
+
+TypeScript
+```typescript
+import lightJoin from 'light-join';
+
+lightJoin('/your-base/url/', 'some/path', '/some-other/path');
+```
+
---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import { resolve } from 'path';
+
+const config = {
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: true,
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    clean: true,
+  },
+};
+
+export default (env: any) => {
+  const isProd = env.NODE_ENV === 'production' || !!env.prod;
+  const mode = isProd ? 'production' : 'development';
+
+  return {
+    ...config,
+    mode,
+  };
+};
